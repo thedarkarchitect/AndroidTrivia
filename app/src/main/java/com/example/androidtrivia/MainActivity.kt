ec9137e86523726bef4102e69d9f28b41e6afe73
thedarkarchitect <romanreynolds240@gmail.com>
2023-05-16 14:39:21 +0300

    first commit

---
+++ app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.androidtrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.androidtrivia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+    }
+}
